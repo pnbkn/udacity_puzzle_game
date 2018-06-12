10eeb0e08bcaac93a947a2efa1b18aece70d0eee
Paul Blackburn <pnblackburn@gmail.com>
2018-06-11 21:04:26 -0700

    major updates

---
+++ README.md
@@ -0,0 +1,34 @@
+# Memory Game
+
+There are 16 cards with 8 matching pairs of symbols. Match all cards to win the game.
+
+## Getting Started
+
+Start by clicking on the first card. Click another card to see if there is a match. Continue this until you have match all cards
+
+## Stars
+
+Start the game with 3 stars. Each card you click is one move. 20 moves and you lose 1 star. 30 moves and lose the 2nd star. 40 moves and lose the last star. 
+
+## Time
+
+The timer will start once you click the first card
+
+## Game Over
+
+Once all cards are match, you will get a score of how many stars you have left, along with the time taken to complete the game.
+
+## Restart
+
+Once the game is over, click restart to shuffle the deck and restart the game.
+
+## Authors
+
+Paul Blackburn
+
+## Acknowledgments
+
+* Card Shuffle - Stack Overflow - https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order
+* Pop Up - Sweet Alert 2 - https://sweetalert2.github.io/
+* Timer - JS Fiddle - https://jsfiddle.net/Daniel_Hug/pvk6p/ 
+
--- js/app.js
+++ js/app.js
@@ -1,161 +1,227 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     function init() {
-       
-     var $ = function(cssClass) { return document.getElementsByClassName(cssClass);};
 
-    // Declare and instantiate variables for classes
-    var deck = $('deck');
-    var card = $('card');
-    var cardFlipped = $('card-flipped');
-    var moves = $('moves');
-    var restart = $('restart');
-    var fa = $('fa');
-    var star = $('fa-star');
-    var repeat = $('fa-repeat');
-    var diamond = $('fa-diamond');
-    var plane = $('fa-paper-plane-o');
-    var anchor = $('fa-anchor');
-    var bolt = $('fa-bolt');
-    var cube = $('fa-cube');
-    var leaf = $('fa-leaf');
-    var bicycle = $('fa-bicycle');
-    var diamond = $('fa-diamond');
-    var cardOne = "";
-    var cardTwo = "";
-    var moveCount = 0;
-    var cardArray = [];
-    var wrongCard = this.className += " card-flipped";
-        
-    // Click to restart the game
-    restart[0].addEventListener('click', restartGame, false);
-        
-    // set up Event Listeners for cards    
-    for (var i = 0; i < card.length; i++) {
-        card[i].addEventListener('click', turnCard, false);
-    }
-    
-    function gameOver() {
-            for (var i = 0; i < card.length; i++) {
-            card[i].removeEventListener('click', turnCard, false);
-       
-            }
-    }
-    
-    function restartGame() {
-        // Reset Star icons
-        for(var s = 0; s < star.length; s++) {
-            star[s].style.opacity = 1;
-        }
-        // Reset game elements
+        var $ = function(cssClass) {
+            return document.getElementsByClassName(cssClass);
+        };
+
+        // Declare and instantiate variables for classes
+        var card = $('card');
+        var cardFlipped = $('card-flipped');
+        var moves = $('moves');
+        var restart = $('restart');
+        var star = $('fa-star');
+        var repeat = $('fa-repeat');
+        var gameTimer = $('gameTimer');
+        var cardOne = ""; // 1st card turned goes here
+        var cardTwo = ""; //
+        var moveCount = 0;
+        var moveCounter = 0;
+        var numMoves = 0;
+        var numClicks = 0; // Whether it is card 1 or card 2
+        var numMatches = 0; // Counts the number of card matches 
+        var starRating = 3; // Counts number of stars left
+        var seconds = 0; // Start timer at 0 seconds
+        var minutes = 0; // Start timer at zero minutees
+        var stopWatch; //
+        var checkTimer = false;
+        var flipped1 = "";
+        var flipped2 = "";
+        var icon1 = "";
+        var icon2 = "";
+        moves[0].textContent = moveCounter;
+        gameTimer[0].textContent = "0" + minutes + ":" + "0" + seconds;
+        // Click to restart the game
+        restart[0].addEventListener('click', gameOver, false);
+
+        // set up Event Listeners for cards    
         for (var i = 0; i < card.length; i++) {
-        card[i].classList.remove('card-flipped');
-        card[i].getElementsByTagName('i')[0].style.opacity = 0;
+            card[i].addEventListener('click', turnCard, false);
+            //console.log(card[1]);
         }
 
-        moves[0].textContent = 3 + " Moves";
-        init();
-    }
-   // Turn Cards 
-        
-    function turnCard() {
-    
-        var element = this.getElementsByTagName('i')[0].className;
-        var liSelected = this.getElementsByTagName('li');
-        var iconSelected = this.getElementsByTagName('i');
-        
-        //this.className += " card-flipped";
-        this.classList.add('card-flipped');
-       //liSelected[0].classList.toggle("card-flipped");
-        var flipCardBack = this;
-        console.log(element);
-        
-        if (moveCount === 0) {
-            cardOne = element;
-            iconSelected[0].style.opacity = 1;
-            moveCount++;
+        // Shuffle Cards - Stack Overflow
+        //https://stackoverflow.com/questions/7070054/javascript-shuffle-html-list-element-order
+        var ul = document.getElementsByTagName('ul')[1];
+        for (var i = ul.children.length; i >= 0; i--) {
+            ul.appendChild(ul.children[Math.random() * i | 0]);
+        }
+
+        //called when game has finished.   
+        function gameOver() {
+
+            console.log(this.className);
+            if (this.className != "restart") {
+                // Pop up when game is over - https://sweetalert2.github.io/
+                swal({
+                        type: 'success',
+                        showCancelButton: true,
+                        cancelButtonText: 'OK',
+                        confirmButtonClass: "primary",
+                        confirmButtonText: "Play Again",
+                        text: 'Your Star Rating is: ' + starRating + ' | Number of Moves: ' + moveCounter + " | Time: " + gameTimer[0].textContent,
+                    },
+                    restartGame()
+
+
+                );
+
+
+            }
+            else {
+                restartGame();
+            }
+
+            for (var i = 0; i < card.length; i++) {
+                card[i].removeEventListener('click', turnCard, false);
+
+            }
+
 
         }
-            
-        else if (moveCount === 1) {
-            cardTwo = element;
-            iconSelected[0].style.opacity = 1;
-            moveCount++;
+        //User clicks refresh icon to reset the game to play again  
+        function restartGame() {
+            // Stop Timer
+            clearTimeout(stopWatch);
+            // Reset Star icons
+            for (var s = 0; s < star.length; s++) {
+                star[s].style.opacity = 1;
+            }
 
-                if(cardOne === cardTwo) {
-                    console.log("You Win");
-                   
-                    moves[0].textContent = "Game Over. You Win!";
-                        for(var s = 0; s < star.length; s++) {
-                        star[s].style.opacity = 0;
-                        }
-                    gameOver();
-                        
-             }
-                else {
-                    console.log("Wrong Selection");
-                    
-                    wrongCard;
-                    star[2].style.opacity = 0;
-                    moves[0].textContent = 2 + " Moves";
-                    
-                  
-                    setTimeout(function(){
-                        flipCardBack.classList.remove('card-flipped');
-                        iconSelected[0].style.opacity = 0; }, 2000);
-                        
+            for (var i = 0; i < card.length; i++) {
+                card[i].classList.remove('card-flipped');
+                card[i].getElementsByTagName('i')[0].style.opacity = 0;
+            }
+            //Reset number of moves text
+
+            init();
+        }
+
+        // Turn Cards     
+        function turnCard() {
+            //Start Timer
+            if (checkTimer === false) {
+                checkTimer = true;
+                gameDuration();
+            }
+
+            starCount(); //Checks the star count on every card click
+            numClicks++;
+            var element = this.getElementsByTagName('i')[0].className;
+            var liSelected = this.getElementsByTagName('li');
+            var iconSelected = this.getElementsByTagName('i');
+            // Flip Card
+            this.classList.add('card-flipped');
+            var flipCardBack = this;
+
+            // Get first card selected        
+            if (numClicks === 1) {
+                cardOne = element;
+                flipped1 = this;
+                icon1 = iconSelected[0];
+                icon1.style.opacity = 1;
+                flipped1.removeEventListener('click', turnCard, false);
+                console.log(flipped1);
+
+            }
+
+            // Get second card selected 
+            else if (numClicks === 2) {
+                cardTwo = element;
+                flipped2 = this;
+                icon2 = iconSelected[0];
+                icon2.style.opacity = 1;
+                moveCounter++;
+                moves[0].textContent = moveCounter;
+                console.log(moveCounter);
+                console.log(flipped2);
+                // If cards match            
+                if (cardOne === cardTwo) {
+                    numClicks = 0;
+                    cardOne = "";
+                    cardTwo = "";
+                    numMatches++;
+                    console.log(numMatches);
+                    flipped2.removeEventListener('click', turnCard, false);
+                    if (numMatches === 8) {
+                        gameOver();
                     }
-    
-        }
-        
-        else if (moveCount === 2) {
-            cardTwo = element;
-            iconSelected[0].style.opacity = 1;
-            moveCount++;
+                }
 
-                if(cardOne === cardTwo) {
-       
-                    moves[0].textContent = "Game Over. You Win!";
-                        for(var s = 0; s < star.length; s++) {
-                        star[s].style.opacity = 0;
-                        }
-                    gameOver();
-                }
-                else {
-                     console.log("Wrong Selection");
-                    star[1].style.opacity = 0;
-                    moves[0].textContent = 1 + " Move";
-                    
-                    setTimeout(function(){
-                        flipCardBack.classList.remove('card-flipped');
-                        iconSelected[0].style.opacity = 0; }, 2000);
-                }
-        }
-        else if (moveCount === 3) {
-            cardTwo = element;
-            iconSelected[0].style.opacity = 1;
-            console.log(moveCount);
-            console.log(cardTwo);
-                if(cardOne === cardTwo) {
-               
-                    star[0].style.opacity = 0;
-                    moves[0].textContent = "Game Over. You Win!";
-                    gameOver();
-                }
-                else {
-                   
-                    star[0].style.opacity = 0;
-                    moves[0].textContent = "You Lose. Try Again!";
-                    gameOver();
-                }
-         }
-        
-            
+                // If cards don't match           
+                else if (cardOne !== cardTwo) {
+                    numClicks = 0;
+                    flipped1.addEventListener('click', turnCard, false);
+                    flipped2.addEventListener('click', turnCard, false);
+                    // Flips wrong card back over to default
+                    setTimeout(function() {
+                        flipped1.classList.remove('card-flipped');
+                        flipped2.classList.remove('card-flipped');
+                        icon1.style.opacity = 0;
+                        icon2.style.opacity = 0;
 
-    } 
-    //console.log(card.length);
-           
-}
-    
-init(); 
-  });
+                    }, 1200);
+
+                }
+
+            }
+
+        }
+
+        // Manages the star count
+        function starCount() {
+            if (moveCounter === 10) {
+                starRating = 2;
+                star[2].style.opacity = 0;
+            } else if (moveCounter === 20) {
+                starRating = 1;
+                star[1].style.opacity = 0;
+            }
+        }
+
+        // Code for timer - JS Fiddle - https://jsfiddle.net/Daniel_Hug/pvk6p/ 
+        function getTime() {
+            seconds++;
+            //Reset seconds and minutes to zero once 60 is reached
+            if (seconds >= 60) {
+                seconds = 0;
+                minutes++;
+
+                if (minutes >= 60) {
+                    minutes = 0;
+
+                }
+            }
+            // If minutes or seconds are under 10, add a zero
+            if (minutes < 10) {
+                if (seconds < 10) {
+                    gameTimer[0].textContent = "0" + minutes + ":" + "0" + seconds;
+                } else {
+                    gameTimer[0].textContent = "0" + minutes + ":" + seconds;
+                }
+
+            } else if (minutes >= 10) {
+
+                if (seconds < 10) {
+                    gameTimer[0].textContent = minutes + ":" + "0" + seconds;
+                } else {
+                    gameTimer[0].textContent = minutes + ":" + seconds;
+                }
+            }
+
+
+            gameDuration();
+        }
+
+        function gameDuration() {
+
+            stopWatch = setTimeout(getTime, 1000);
+            console.log(seconds);
+        }
+
+
+    }
+
+    init();
+});
